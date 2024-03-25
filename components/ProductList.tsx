6ef4e951bd367181f6b35e48f6b701a41b60e6dc
이용현 <yh9035926@naver.com>
2024-03-26 08:53:59 +0900

    useInfiniteQuery

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -2,12 +2,20 @@ import { Product } from "@/graphql/products";
 import React from "react";
 import ProductItem from "./ProductItem";
 
-const ProductList = ({ list }: { list: Product[] }) => {
+const ProductList = ({
+  list,
+}: {
+  list: {
+    products: Product[];
+  }[];
+}) => {
   return (
     <ul className="productList">
-      {list.map((product) => (
-        <ProductItem {...product} key={product.id} />
-      ))}
+      {list.map((page) =>
+        page.products.map((product) => (
+          <ProductItem {...product} key={product.id} />
+        ))
+      )}
     </ul>
   );
 };
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,19 +1,36 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery, useQuery } from "react-query";
 import { QueryKeys, graphqlFetcher } from "@/queryClient";
 import React from "react";
 import GET_PRODUCTS, { Products } from "@/graphql/products";
 import ProductList from "@/components/ProductList";
 
 const ProductListPage = () => {
-  const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
-    graphqlFetcher<Products>(GET_PRODUCTS)
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
+    QueryKeys.PRODUCTS,
+    ({ pageParam = "" }) =>
+      graphqlFetcher<Products>(GET_PRODUCTS, { cursor: pageParam }),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.products.at(-1)?.id;
+      },
+    }
   );
+
+  /* 
+data: {
+  {pageParams : [undefind...]},
+  {pages: [
+    {products:[{...}]},
+    {products:[{...}]}
+  ]}
+}
+ */
 
   return (
     <div>
       <div className="productWrapper">
         <div className="productsTitle">상품 목록</div>
-        <ProductList list={data?.products || []} />
+        <ProductList list={data?.pages || []} />
       </div>
     </div>
   );
