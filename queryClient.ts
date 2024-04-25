fcf010fbca3e931d3863036e45f1509fc472fb64
yh9035926 <yh9035926@naver.com>
2024-04-26 05:55:48 +0900

    http:추가

--- queryClient.ts
+++ queryClient.ts
@@ -23,7 +23,7 @@ export const getClient = (() => {
   };
 })();
 
-const BASE_URL = "nosy-hedgehog-tomyhas59.koyeb.app/graphql";
+const BASE_URL = "https://nosy-hedgehog-tomyhas59.koyeb.app/graphql";
 
 /* export const restFetcher = async ({
   method,
