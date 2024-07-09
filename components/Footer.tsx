82445bade9f7a3ecaa9ed4f5e51a524498f7dab5
yh9035926 <yh9035926@naver.com>
2024-07-09 11:38:22 +0900

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
       </div>
       <div className="footerInfo">
         <div className="footerList">이메일: yh9035926@naver.com</div>
-        <div className="footerList">전화번호: 010-7170-5926</div>
+        <div className="footerList">전화번호: 010-1234-1234</div>
         <div className="footerList">© 2024 MyShop. All rights reserved.</div>
       </div>
     </div>
