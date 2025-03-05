3ea80da17d0a769359e76deb5df8194e380e296c
tomyhas59 <yh9035926@naver.com>
2025-03-06 00:07:30 +0900

    change password

--- pages/userInfo/index.tsx
+++ pages/userInfo/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useUser } from "@/context/UserProvider";
 
 const UserInfoPage: React.FC = () => {
   const { user } = useUser();
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
   return (
     <div className="user-info-page">
@@ -21,6 +25,53 @@ const UserInfoPage: React.FC = () => {
       ) : (
         <p className="loading-text">Loading...</p>
       )}
+      <form className="change-password-form">
+        <div className="form-group">
+          <label htmlFor="old-password" className="form-label">
+            이전 비밀번호
+          </label>
+          <input
+            id="old-password"
+            className="form-input"
+            type="password"
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+            placeholder="이전 비밀번호를 입력하세요"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="new-nassword" className="form-label">
+            새 비밀번호
+          </label>
+          <input
+            id="new-password"
+            className="form-input"
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            placeholder="새 비밀번호를 입력하세요"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordConfirm" className="form-label">
+            비밀번호 확인
+          </label>
+          <input
+            id="password-confirm"
+            className="form-input"
+            type="password"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            placeholder="비밀번호를 확인하세요"
+            required
+          />
+        </div>
+        <button className="sign-button" type="submit">
+          비밀번호 변경
+        </button>
+      </form>
     </div>
   );
 };
